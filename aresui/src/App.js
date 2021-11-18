8214b3b1a6266a1a9d40628a431ca27afdeaf78c
Aditya <aditya@Adityas-MacBook-Air.local>
2021-11-18 14:28:06 +0530

    Edited

---
+++ aresui/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import ImageUpload from './Components/Fileupload';
+
+
+function App() {
+  
+  return (
+    <div>
+    <div class="grid">
+  <div class="g-col-6">  
+    <div className="App">
+      <ImageUpload/>
+    </div>
+  </div>
+</div>
+
+  </div>
+  );
+}
+
+export default App;
