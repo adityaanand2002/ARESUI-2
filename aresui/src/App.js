93c77af2dc67ee1fa9f57441bba010e788311eb4
Aditya <aditya@Adityas-MacBook-Air.local>
2021-11-18 15:41:26 +0530

    Table Added

--- aresui/src/App.js
+++ aresui/src/App.js
@@ -2,6 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import ImageUpload from './Components/Fileupload';
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 
 function App() {
   
@@ -20,3 +28,44 @@ function App() {
 }
 
 export default App;
+
+
+function createData(name, element) {
+  return { name, element};
+}
+
+const rows = [
+  createData('goethite', 'FeOOH'),
+  createData('Quartz', 'SiO2'),
+  createData('K-feldspar', 'KAlSi3O8'),
+  createData('Albite', 'NaAlSi3O8'),
+  createData('Calcium Aluminosilicate', 'CaAl2Si2O8'),
+];
+
+export default function BasicTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name of Mineral</TableCell>
+            <TableCell align="right">Elements/Compunds</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.element}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
